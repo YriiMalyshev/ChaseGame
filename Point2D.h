95ac422fd2293a9bcd3d50a2b44445c8c2597599
Malyshev yuriy <yuaren@yandex.ru>
2023-04-08 23:31:04 +0300

    changed clas Point2D: added methods: getX, getY;

--- Point2D.h
+++ Point2D.h
@@ -4,25 +4,29 @@ using namespace std;
 
 class Point2D {
 private:
+
     int x;
     int y;
 public:
 
-    // Конструктор
-    Point2D(int x = 0, int y = 0) : x(x), y(y) {  }
 
-    // Перегрузка оператора вывода
-    friend std::ostream& operator<<(std::ostream& out, const Point2D& point) {
-        out << "(" << point.x << ", " << point.y << ")";
-        return out;
-    }
+    Point2D(int x = 0, int y = 0) : x(x), y(y) {  }
 
     void setPoint(int x, int y) {
         this->x = x;
         this->y = y;
     }
+
+    int getX() { return x; }
+    int getY() { return y; }
+
     bool operator==(const Point2D& point) {
         if (x == point.x && y == point.y) return 1;
         else return 0;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Point2D& point) {
+        out << "(" << point.x << ", " << point.y << ")";
+        return out;
     }
 };
