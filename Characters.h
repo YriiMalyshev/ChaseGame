23d47414b6045b98def4305c857480f001d85fef
Malyshev yuriy <yuaren@yandex.ru>
2023-04-09 20:32:07 +0300

    Refactoring methods in class. Add comments to each method and function in the code for better readability and maintainability

--- Characters.h
+++ Characters.h
@@ -1,5 +1,4 @@
-﻿
-#pragma once
+﻿#pragma once
 
 #include "Point2D.h"
 
@@ -7,20 +6,20 @@ class Character {
 
 private:
 
-    string name;
-    Point2D location;
-    bool npc;
+    string name;        // private member variable to store the character's name
+    Point2D location;   // private member variable to store the character's current location
+    bool npc;           // private member variable to indicate whether the character is controlled by the computer (non-player character or NPC)
 
 public:
 
     Character(const std::string& name, const Point2D& location, bool npcFlag = 0)
-        : name(name), location(location), npc(npcFlag) {   }
+        : name(name), location(location), npc(npcFlag) {   }  // constructor for the Character class which initializes the private member variables
 
-    void MoveTo(int direction, int steps) {
+    void moveTo(int direction, int steps) {  // method to move the character in a specific direction for a certain number of steps
 
-        int deltaX(0), deltaY(0);
+        int deltaX(0), deltaY(0);   // local variables to store the change in x and y coordinates
 
-        switch (direction)
+        switch (direction)  // switch statement to determine the change in x and y coordinates based on the specified direction
         {
         case 0:
             deltaY = 1;
@@ -54,117 +53,49 @@ public:
             break;
         }
 
-        location.setPoint(location.getX() + deltaX * steps, location.getY() + deltaY * steps);
+        location.setPoint(location.getX() + deltaX * steps, location.getY() + deltaY * steps);  // update the character's location based on the change in x and y coordinates
     }
 
-    Point2D getLocation() {
+    Point2D getLocation() {   // method to retrieve the character's current location
         return location;
     }
 
-    bool isNPC() { return npc; }
+    bool isNPC() { return npc; }   // method to determine whether the character is an NPC
 
-    virtual void AutoMove() = 0;
+    virtual void autoMove() = 0;   // virtual method to be implemented in derived classes, used to move the character automatically
 
 };
-
 
 class Prey : Character {
 
 private:
 
-
-    const int range = 1;
-
-public:
-    Prey(const std::string& name, const Point2D& location, bool npcFlag = 0) : Character(name, location, npcFlag) {   }
-
-    void AutoMove() override;
-
-
-};
-
-
-
-class Predator : Character {
-private:
-
-
-    int range;
+    const int maxRange = 1;  // private member variable to store the maximum range that the Prey can move in a single turn
 
 public:
-    Predator(const std::string& name, const Point2D& location, bool npcFlag = 0)
-        : Character(name, location, npcFlag), range(0) {   }
 
+    Prey(const std::string& name, const Point2D& location, bool npcFlag = 0) : Character(name, location, npcFlag) {   }  // constructor for the Prey class which calls the base class constructor
 
-    void AskRange() {
-        do {
-            cout << "Íà ñêîëüêî? (1-5)" << endl;
-            cin >> range;
-            if (1 <= range && range <= 5) break;
-        } while (true);
-    }
+    int askDirection() {   // method to ask the user for a direction to move in
 
-    void AutoMove();
-};
-
-
-
-
-void Prey::AutoMove() {
-
-    int direction;
-
-    if (isNPC()) {
-        direction = rand() % 8;
-    }
-    else {
-        cout << "Êóäà èäòè?\n0-ââåðõ-âëåâî 1-ââåðõ 2-ââåðõ-âïðàâî 3-âëåâî 4-âïðàâî 5-âíèç-âëåâî 6-âíèç 7-âíèç-âïðàâî" << endl;
+        int direction(0);
+        cout << "Куда пойти?" << endl;
+        cout << "0 - вверх, 1 - вправо, 2 - вниз, 3 - влево," << endl;
+        cout << "4 - вверх-влево, 5 - вверх-вправо, 6 - вниз-вправо, 7 - вниз-влево" << endl;
         cin >> direction;
     }
 
-    MoveTo(direction, range);
-}
-
-
-
-void Predator::AutoMove() {
-
-    int direction;
-
-    if (isNPC()) {
-        direction = rand() % 4;
-        SetRange(rand() % 5 + 1);
-    }
-    else {
-        int s = 0;
-        cout << "Êóäà èäòè?\n0-ââåðõ 1-âïðàâî 2-âíèç 3-âëåâî" << endl;
-        cin >> direction;
-    }
-
-    switch (q) {
-    case 0:
-        if (location.y >= (a)) {
-            location.y -= a;
+    void autoMove() override {
+        int direction = 0;
+        int range = 0;
+        if (isNPC()) {
+            direction = rand() % 4;
+            range = rand() % 5 + 1;
         }
-        else cout << "Âûõîä çà ïðåäåëû" << endl;
-        break;
-    case 1:
-        if (location.x <= (ar.w - a)) {
-            location.x += a;
+        else {
+            direction = askDirection();
+            range = askRange();
         }
-        else cout << "Âûõîä çà ïðåäåëû" << endl;
-        break;
-    case 2:
-        if (location.y <= (ar.l - a)) {
-            location.y += a;
-        }
-        else cout << "Âûõîä çà ïðåäåëû" << endl;
-        break;
-    case 3:
-        if (location.x >= (a)) {
-            location.x -= a;
-        }
-        else cout << "Âûõîä çà ïðåäåëû" << endl;
-        break;
-    }
+        moveTo(direction, range);
+    }  // if the Prey is an NPC, choose a random direction to move in
 }
