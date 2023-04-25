2351f7abf28d89d5ea569ee77d9ac55813c49e2a
Malyshev yuriy <yuaren@yandex.ru>
2023-04-25 15:34:30 +0300

    added comments to Characters method

--- Characters.cpp
+++ Characters.cpp
@@ -2,21 +2,21 @@
 
 using namespace std;
 
-Character::Character(const std::string& name, const Point2D& location, bool npcFlag)
-    : name(name), location(location), npc(npcFlag) {   }
+// Конструктор класса Character
+Character::Character(const string& name, const Point2D& location, bool npcFlag)
+    : name(name), location(location), npc(npcFlag) {}
 
+// Метод для перемещения персонажа в указанную точку
 void Character::moveTo(Point2D point) {
     location = point;
 }
 
-//0 - ����� 1 - ������ 2 - ���� 3 - ����� 4 - �����-����� 
-//5 - �����-������ 6 - ����-������ 7 - ����-�����
+// Метод для перемещения персонажа в направлении direction на указанное количество шагов steps
 void Character::moveTo(int direction, int steps) {
+    int deltaX = 0, deltaY = 0;
 
-    int deltaX(0), deltaY(0);
-
-    switch (direction)
-    {
+    // Определяем изменение координат в зависимости от направления движения
+    switch (direction) {
     case 0:
         deltaY = 1;
         break;
@@ -49,40 +49,43 @@ void Character::moveTo(int direction, int steps) {
         break;
     }
 
+    // Обновляем координаты персонажа
     location.setPoint(location.getX() + deltaX * steps, location.getY() + deltaY * steps);
 }
 
+// Метод для получения текущей позиции персонажа
 Point2D Character::getLocation() {
     return location;
 }
 
-bool Character::isNPC() { return npc; }
+// Метод для проверки, является ли персонаж NPC
+bool Character::isNPC() {
+    return npc;
+}
 
 
+// Конструктор класса Prey
+Prey::Prey(const string& name, const Point2D& location, bool npcFlag)
+    : Character(name, location, npcFlag) {}
 
-
-
-
-Prey::Prey(const std::string& name, const Point2D& location, bool npcFlag) : Character(name, location, npcFlag) {   }
-
+// Метод для запроса направления у пользователя
 int Prey::askDirection() {
-
     do {
-
-        int direction(0);
-        cout << "���� ����? \n";
-        cout << "0 - �����, 1 - ������, 2 - ����, 3 - �����, \n";
-        cout << "4 - ����� - �����, 5 - �����-������, 6 - ����-������, 7 - ����-����� \n";
+        int direction = 0;
+        cout << "Куда вы хотите пойти? \n";
+        cout << "0 - вверх, 1 - вправо, 2 - вниз, 3 - влево, \n";
+        cout << "4 - вверх-влево, 5 - вверх-вправо, 6 - вниз-вправо, 7 - вниз-влево \n";
         cin >> direction;
 
         if (direction <= 7 && direction >= 0) {
             return direction;
         }
-        else cout << "������������ ����, �������� ��� ��� \n" << endl;
-
+        else {
+            cout << "Неверное направление, попробуйте еще раз \n" << endl;
+        }
     } while (true);
-
 }
+
 
 void Prey::autoMove() {
 
